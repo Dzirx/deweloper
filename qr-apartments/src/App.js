2a56c630a2e78b9402d3380761f3183a5174b35e
Mateusz Dziura <mateusz.dziura@assecobs.pl>
2024-09-09 19:41:23 +0200

    Second commit

--- qr-apartments/src/App.js
+++ qr-apartments/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import AddApartament from './components/AddApartment.js';
+import QRGenerator from './components/GenerateQR.js';
+import QRScanner from './components/ScanQR.js';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/repair-form" element={<AddApartament />} />
+          <Route path="/qr-generator" element={<QRGenerator />} />
+          <Route path="/qr-scanner" element={<QRScanner />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Strona główna</h1>
+      <Link to="/repair-form">
+        <button>Dodaj mieszkanie</button>
+      </Link>
+      <br/>
+      <Link to="/qr-generator">
+        <button>Generator kodów QR</button>
+      </Link>
+      <br/>
+      <Link to="/qr-scanner">
+        <button>Skaner kodów QR</button>
+      </Link>
     </div>
   );
 }
